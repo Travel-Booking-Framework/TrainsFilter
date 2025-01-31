3b9c6cfddaf10fe6475d6f582bb8dc4020d4137b
Ali Ahmadi <aliahmadi79sh@gmail.com>
2025-01-31 14:11:07 +0330

    Install Packages for elasticsearch + Basic Configs in Settings

--- TrainsFilter/settings.py
+++ TrainsFilter/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_persian',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_elasticsearch_dsl',
+    'TFilter',
 ]
 
 MIDDLEWARE = [
@@ -74,11 +77,10 @@ WSGI_APPLICATION = 'TrainsFilter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+ELASTICSEARCH_DSL = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'hosts': ['localhost:9200'],  # Elasticsearch server address
+    },
 }
 
 
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-ir'
 
 TIME_ZONE = 'UTC'
 
